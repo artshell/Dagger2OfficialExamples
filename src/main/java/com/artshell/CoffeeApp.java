bce48c9a7d0150bf5efcebe7ce0d1b6e97fbc713
artshell <872780008.qq.com>
2018-10-30 23:37:52 +0800

    Modify: update configuration

--- src/main/java/com/artshell/CoffeeApp.java
+++ src/main/java/com/artshell/CoffeeApp.java
@@ -5,6 +5,7 @@ import dagger.Component;
 import javax.inject.Singleton;
 
 public class CoffeeApp {
+
     @Singleton
     @Component(modules = DripCoffeeModule.class)
     public interface Coffee {
